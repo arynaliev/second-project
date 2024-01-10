7c8c62db75113a3fb22f482ac0c2c810c818aa18
Azim <azimarynaliev@icloud.com>
2024-01-09 20:12:49 -0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import logo from "./illusion-image.webp";
+// import logo from "./illusion-image.webp";
 import "./App.css";
+
+const boysNames = ["Azim", "Tima", "Dastan"];
+const girlsNames = ["Meka", "Aida", "Aigerim", "Janara", "Erkinai", "Alina"];
+
+const showStudentsNames = (array) => {
+  return array.map((el, index) => (
+    <div
+      style={{
+        border: "1px solid black",
+        margin: "10px",
+        padding: "10px",
+        width: "100px",
+      }}
+    >
+      <span key={index}>{el}</span>
+    </div>
+  ));
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="boys">{showStudentsNames(boysNames)}</div>
+      <hr />
+      <div className="girls">{showStudentsNames(girlsNames)}</div>
     </div>
   );
 }

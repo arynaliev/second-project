4254795332cb60523107e3a19e1d585c11e061fe
Azim <azimarynaliev@icloud.com>
2024-01-09 18:37:19 -0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import logo from "./illusion-image.webp";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Helloo!</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     </div>
   );
 }
